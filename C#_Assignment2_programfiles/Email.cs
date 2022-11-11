84abef249c9ba55154416468fde53e41c5dff6d8
sakshisapkale <sakshi.sapkale@emtecinc.com>
2022-11-11 15:10:39 +0530

    add c#  assignment 2

---
+++ C#_Assignment2_programfiles/Email.cs
@@ -0,0 +1,40 @@
+﻿
+namespace ConsoleApp4_assign1_Q4
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Text.RegularExpressions;
+    using System.Threading.Tasks;
+    internal class Program
+    {
+        public static bool EmailIsValid(string email)
+        {
+            string expression = "\\w+([-+.']\\w+)*@\\w+([-.]\\w+)*\\.\\w+([-.]\\w+)*";
+
+            if (Regex.IsMatch(email, expression))
+            {
+                if (Regex.Replace(email, expression, string.Empty).Length == 0)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter any Email :");
+            string email = Console.ReadLine();
+            if (EmailIsValid(email))
+            {
+                Console.WriteLine("Email is valid");
+            }
+            else
+            {
+                Console.WriteLine("Email is not valid");
+            }
+            Console.ReadLine();
+        }
+    }
+}
