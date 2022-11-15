9aecd3d828099dafd4720590f192ea5d061d7342
sakshisapkale <sakshi.sapkale@emtecinc.com>
2022-11-15 11:13:25 +0530

    add .net assignments

---
+++ WebApp/WebAdd/Controllers/HomeController.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using WebAdd.Models;
+
+namespace WebAdd.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Index(calculator cal)
+        {
+            int a, b;
+            a = cal.value1;
+            b = cal.value2;
+           
+            if (cal.calculate == "sub")
+            {
+                cal.result = a - b;
+            }
+            else if (cal.calculate == "mul")
+            {
+                cal.result = a * b;
+            }
+            else if (cal.calculate == "div")
+            {
+                cal.result = a/b;
+            }
+            else
+            {
+                cal.result = a + b;
+            }
+            ViewData["result"]=cal.result;
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ WebApp/WebAdd/Models/calculator.cs
@@ -0,0 +1,12 @@
+﻿namespace WebAdd.Models
+{
+    public class calculator
+    {
+        public int value1 { get; set; }
+        public int value2 { get; set; }
+
+        public int result { get; set; }
+        public string calculate { get; set; }
+
+    }
+}
