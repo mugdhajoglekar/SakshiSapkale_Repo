cdcda81ac8e3b2942df3fe8f163bb8d0be7863f3
sakshisapkale <sakshi.sapkale@emtecinc.com>
2022-11-22 22:01:26 +0530

    add dapper web api

---
+++ DapperWebApi/DapperWebApi/Controllers/SuperHeroesController.cs
@@ -0,0 +1,72 @@
+﻿namespace DapperWebApi.Controllers
+{
+    using Dapper;
+    using Microsoft.AspNetCore.Http;
+    using Microsoft.AspNetCore.Mvc;
+    using System.Data.SqlClient;
+    using System.Configuration;
+    using DapperWebApi.Inventory;
+    using DapperWebApi.Models;
+
+    [Route("api/[controller]")]
+    [ApiController]
+
+    public class SuperHeroesController : ControllerBase
+    {
+        //Access QuriesRepository
+        QueriesRepository db = new QueriesRepository();
+
+        private readonly IConfiguration _config;
+
+        public SuperHeroesController(IConfiguration config)
+        {
+            _config = config;
+        }
+
+        //Read all records
+        [HttpGet]
+        public ActionResult GetAllSuperHeroes()
+        {
+            using var connection = new SqlConnection(_config.GetConnectionString("DefaultConnection"));
+            var output = db.GetAll(connection);
+            return Ok(output);
+        }
+
+        //Get record
+        [HttpGet("{heroId}")]
+        public ActionResult GetHero(int heroId)
+        {
+            using var connection = new SqlConnection(_config.GetConnectionString("DefaultConnection"));
+            var output = db.GetSingleItem(connection,heroId);
+            return Ok(output);
+        }
+
+        //Create
+        [HttpPost]
+        public ActionResult CreateHero(SuperHero hero)
+        {
+            using var connection = new SqlConnection(_config.GetConnectionString("DefaultConnection"));
+            db.addHero(connection,hero);
+            return Ok(db.GetAll(connection));
+        }
+
+        //Update 
+        [HttpPut]
+        public ActionResult UpdateHero(SuperHero hero)
+        {
+            using var connection = new SqlConnection(_config.GetConnectionString("DefaultConnection"));
+            db.editHero(connection, hero);
+            return Ok(db.GetAll(connection));
+        }
+
+        //delete
+        [HttpDelete("{heroId}")]
+        public ActionResult DeleteHero(int heroId)
+        {
+            using var connection = new SqlConnection(_config.GetConnectionString("DefaultConnection"));
+            db.removeHero(connection, heroId);
+            return Ok(db.GetAll(connection));
+        }
+
+    }
+}
---
+++ DapperWebApi/DapperWebApi/Repository/QueriesRepository.cs
@@ -0,0 +1,34 @@
+﻿namespace DapperWebApi.Inventory
+{
+    using System.Collections.Generic;
+    using System.Data.SqlClient;
+    using Dapper;
+    using DapperWebApi.Models;
+
+    public class QueriesRepository 
+    {
+        public IEnumerable<SuperHero> GetAll(SqlConnection connection)
+        {
+            return connection.Query<SuperHero>("select * from SuperHeroes");
+        }
+        public IEnumerable<SuperHero> GetSingleItem(SqlConnection connection,int heroId)
+        {
+            return connection.Query<SuperHero>("select * from SuperHeroes where id=@Id", new { Id = heroId });
+        }
+        public IEnumerable<SuperHero> addHero(SqlConnection connection, SuperHero hero)
+        {
+            return connection.Query<SuperHero>("insert into SuperHeroes(id,name,firstname,lastname,place) values(@Id, @Name, @FirstName, @LastName, @Place)",
+                new { Id = hero.Id, Name=hero.Name ,FirstName=hero.FirstName, LastName=hero.LastName, Place=hero.Place,});
+        }
+        public IEnumerable<SuperHero> editHero(SqlConnection connection, SuperHero hero)
+        {
+            return connection.Query<SuperHero>("update SuperHeroes set id=@Id, name=@Name, firstname=@FirstName, lastname=@LastName, place=@Place where id=@Id",
+                new { Id = hero.Id, Name = hero.Name, FirstName = hero.FirstName, LastName = hero.LastName, Place = hero.Place, });
+        }
+        public IEnumerable<SuperHero> removeHero(SqlConnection connection, int heroId)
+        {
+            return connection.Query<SuperHero>("delete from SuperHeroes where id=@Id", new { Id = heroId });
+        }
+
+    }
+}
